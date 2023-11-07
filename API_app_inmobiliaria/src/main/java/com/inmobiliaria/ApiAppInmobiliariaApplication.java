6fb7049f577290bcf6f94d6473f49d41bf36d1e7
RobertoCarrera <robertocarrera93@gmail.com>
2023-11-07 22:55:25 +0100

    final

--- API_app_inmobiliaria/src/main/java/com/inmobiliaria/ApiAppInmobiliariaApplication.java
+++ API_app_inmobiliaria/src/main/java/com/inmobiliaria/ApiAppInmobiliariaApplication.java
@@ -1,13 +1,20 @@
 package com.inmobiliaria;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiAppInmobiliariaApplication {
+public class ApiAppInmobiliariaApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiAppInmobiliariaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Iniciando proceso");
 	}
 
 }
---
+++ API_app_inmobiliaria/src/main/java/com/inmobiliaria/CorsConfig.java
@@ -0,0 +1,9 @@
+package com.inmobiliaria;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class CorsConfig {
+
+}
